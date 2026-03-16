7f5ad54f37f60e79cc5e4be74a88361ac2f95656
shamthecodes <shamthecoder@gmail.com>
2026-03-16 06:14:42 +0530

    dom-cursor-event

---
+++ dom3.js
@@ -0,0 +1,14 @@
+var imgcard = document.querySelector("#container");
+
+var btnlove = document.querySelector("i");
+
+imgcard.addEventListener("dblclick", () => {
+  btnlove.style.transform = "translate(-50%, -50%) scale(1)";
+  btnlove.style.opacity = 0.8;
+  setTimeout(() => {
+    btnlove.style.opacity = "0";
+  }, 1000);
+  setTimeout(() => {
+    btnlove.style.transform = "translate(-50%, -50%) scale(0)";
+  }, 1000);
+});
